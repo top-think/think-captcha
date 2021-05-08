e73ba44fd24fb1633cc006db24efbde4a460467f
rise <rise.worlds@outlook.com>
2021-05-08 11:58:16 +0800

    Update Captcha.php

--- src/Captcha.php
+++ src/Captcha.php
@@ -12,8 +12,6 @@
 namespace think\captcha;
 
 use think\facade\Session;
-use think\facade\Cache;
-use think\facade\App;
 
 class Captcha
 {
@@ -40,7 +38,7 @@ class Captcha
         // 验证码图片高度
         'imageW'   => 0,
         // 验证码图片宽度
-        'length'   => 4,
+        'length'   => 5,
         // 验证码位数
         'fontttf'  => '',
         // 验证码字体，不设置随机获取
@@ -112,21 +110,18 @@ class Captcha
     {
         $key = $this->authcode($this->seKey) . $id;
         // 验证码不能为空
-        // $secode = Session::get($key, '');
-        $secode = Cache::get($key);
+        $secode = Session::get($key, '');
         if (empty($code) || empty($secode)) {
             return false;
         }
         // session 过期
         if (time() - $secode['verify_time'] > $this->expire) {
-            // Session::delete($key, '');
-            Cache::rm($key);
+            Session::delete($key, '');
             return false;
         }
 
         if ($this->authcode(strtoupper($code)) == $secode['verify_code']) {
-            // $this->reset && Session::delete($key, '');
-            $this->reset && Cache::rm($key);
+            $this->reset && Session::delete($key, '');
             return true;
         }
 
@@ -205,20 +200,7 @@ class Captcha
         $secode                = [];
         $secode['verify_code'] = $code; // 把校验码保存到session
         $secode['verify_time'] = time(); // 验证码创建时间
-        // Session::set($key . $id, $secode, '');
-        //保存验证码到缓存
-        $options = [
-            // 缓存类型为File
-            'type'  =>  'File',
-            // 缓存有效期为永久有效
-            'expire'=>  0,
-            //缓存前缀
-            'prefix'=>  'think',
-            // 指定缓存目录
-            'path'  =>  App::getAppPath().'runtime/cache/',
-        ];
-        Cache::connect($options);
-        Cache::set($key . $id, $secode, $this->expire);
+        Session::set($key . $id, $secode, '');
 
         ob_start();
         // 输出图像
